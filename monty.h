b998dc7551a40687f90dbcf204e14b0b7aeb52e8
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-03-23 16:34:15 +0100

    'unused'

--- monty.h
+++ monty.h
@@ -38,7 +38,7 @@ typedef struct instruction_s
 
 /* Function prototypes */
 void (*get_instruction(char *line, unsigned int line_number))(stack_t **stack, unsigned int line_number);
-void *push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 
 /* Monty File Pointer */
