878a6d519c2b4d75d0869d15aff2962102a2a776
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-24 00:51:54 +0100

    add

---
+++ m_instructions.c
@@ -0,0 +1,21 @@
+#include"monty.h"
+/**
+ * add -  add the top two elements of the stack
+ * @stack: pointer to pointer to stack
+ * @line_number: line number
+ * Return: nothing
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = *stack;
+    tmp->next->n += tmp->n;
+	*stack = tmp->next;
+	free(tmp);
+}
