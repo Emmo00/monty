b454cbbd6fbe0fc7c13303d62f05539893b6b4bb
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-23 21:04:20 +0100

    betty

--- instructions.c
+++ instructions.c
@@ -57,6 +57,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
 }
