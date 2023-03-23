b977a2f65ae57033ad0989d341a8fc33eba13e8e
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-23 19:15:54 +0100

    unused

--- instructions.c
+++ instructions.c
@@ -7,6 +7,7 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
+	(void) line_number;
 	stack_t *node;
 
 	node = malloc(sizeof(stack_t));
@@ -30,9 +31,9 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	(void) line_number;
 	unsigned int len;
 	stack_t *node;
+	(void) line_number;
 
 	len = 0;
 	node = *stack;
